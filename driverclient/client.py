0cb53279f524e3e996a8cd2b1ed8af6cbfdf561f
Drew Pihera <drew.pihera@gtri.gatech.edu>
2016-03-06 19:19:11 -0500

    very basic ability to talk to both the wsclient and the api server at the same time

--- driverclient/client.py
+++ driverclient/client.py
@@ -3,22 +3,26 @@ Adapted from: https://docs.python.org/3.4/howto/sockets.html
 """
 
 import socket
+from tornado import (
+    gen,
+)
 
-class DriverSocket:
+class DriverClient:
     """
-    Socket for talking to the c driver socket server.
-    TODO: Currently blocking, make non-blocking
+    Client for talking to the c driver socket server.
     TODO: Send/receive both need MSGLEN, and need to format the msgs right
           (bytes-like objects).
+    TODO: Clean up use of coroutines vs callbacks (everywhere)
     """
 
     def __init__(self, sock=None):
         """
+        Create the driver socket
+        sock: An already created socket to use
         """
         if sock is None:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # TODO: set non-blocking and use select to determine when to read
-            # socket.setblocking(0)
+            self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         else:
             self.sock = sock
 
@@ -26,27 +30,34 @@ class DriverSocket:
         """
         Connect to the driver's socket.
 
-        :param str host: Host IP address
-        :param int port: Port to connect to
+        host: Host IP address
+        port: Port to connect to
         """
         self.sock.connect((host, port))
+        # TODO: set non-blocking and use select to determine when to read
+        # socket.setblocking(0)
 
+    # @gen.coroutine
     def drvsend(self, msg):
         """
         Send to the driver.
-        :param str msg: String message to convert to a bytes-like object and
-                        send to the server.
+        msg: String message to convert to a bytes-like object and send to the
+             server.
         """
         totalsent = 0
-        # for now this is a string, so just encode it and send
-        sent = self.sock.send(msg.encode())
 
+        # TODO: for now this is a string, so just encode it and send. make more
+        #       robust
         # while totalsent < MSGLEN:
         #     sent = self.sock.send(msg[totalsent:])
         #     if sent == 0:
         #         raise RuntimeError("socket connection broken")
         #     totalsent = totalsent + sent
+        sent = self.sock.send(msg.encode())
 
+        return sent
+
+    # @gen.coroutine
     def drvreceive(self):
         """
         Receive from the driver.
@@ -56,9 +67,6 @@ class DriverSocket:
         # TODO: hack so MSGLEN is defined. fix
         MSGLEN = 2048
         # TODO: get chunked read working
-        ret = self.sock.recv(2048)
-        print(ret)
-        return ret
         # while bytes_recd < MSGLEN:
         #     chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
         #     if chunk == b'':
@@ -67,6 +75,18 @@ class DriverSocket:
         #     bytes_recd = bytes_recd + len(chunk)
         # return b''.join(chunks)
 
+        ret = self.sock.recv(2048)
+
+        return ret
+
+
+    def close(self):
+        """
+        Close our socket.
+        """
+        self.sock.close()
+
+    # TODO: just for testing, remove
     def test_echo(self):
         self.connect("127.0.0.1", 60002)
         self.drvsend("test")
--- server_main.py
+++ server_main.py
@@ -2,21 +2,63 @@ from tornado import (
     web,
     httpserver,
     ioloop,
+    gen,
 )
 
 from wsserver.server import WSHandler
+from driverclient.client import DriverClient
 
 application = web.Application([
     (r'/ws', WSHandler),
 ])
 
+@gen.coroutine
+def process_drv_socket(sock):
+    """
+    Periodically called function for processing the api driver's socket
+    """
+    sock.drvsend('test')
+    yield sock.drvreceive()
+
+# how fast should we process the driver socket (ms)
+LINK_RATE = 2000
+# how fast should we time out when processing the driver socket (ms)
+LINK_TIMEOUT = 3000
+
+WS_SERV_PORT = 8675
+API_SERV_PORT = 60002
+API_SERV_IP = '127.0.0.1'
 
 if __name__ == "__main__":
     http_server = httpserver.HTTPServer(application)
-    http_server.listen(8675)
+    http_server.listen(WS_SERV_PORT)
+
+    # assume nothing fails here...
+    drvsock = DriverClient()
+    drvsock.connect(API_SERV_IP, API_SERV_PORT)
+
     try:
-        print('Starting IOLoop with http server for websockets')
+        print(
+            'Starting IOLoop with http server for websockets and driver socket'
+        )
+
+        # TODO: if coroutines...
+        # TODO: works for a single loop, then stops. figure out how to keep
+        #       going.
+        # NOTE: don't mix this with a call to IOLoop.instance().start()
+        # ioloop.IOLoop.instance().run_sync(lambda: process_drv_socket(drvsock))
+
+        # TODO: this is using the callback style. other things use the
+        #       coroutine style. pick one.
+        # background processs of driver socket every LINK_RATE milliseconds
+        drvsock_task = ioloop.PeriodicCallback(lambda: process_drv_socket(drvsock), LINK_RATE)
+        drvsock_task.start()
+
+        # start the ioloop
         ioloop.IOLoop.instance().start()
+
     except KeyboardInterrupt:
         http_server.stop()
+        drvsock_task.stop()
+        drvsock.close()
         print('KeyboardInterrupt: Killed the server')
