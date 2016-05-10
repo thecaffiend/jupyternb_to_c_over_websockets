27ec7d8cc7926b267bc0240d273f81b8e7d89309
Drew Pihera <drew.pihera@gtri.gatech.edu>
2016-05-09 21:33:08 -0400

    added printBuffer and processBuffer functions. now use these on the incoming buffer instead of just echoing

--- c_driver/driver.c
+++ c_driver/driver.c
@@ -9,12 +9,50 @@
 #include <sys/socket.h>
 #include <resolv.h>
 #include <arpa/inet.h>
-#include <errno.h>
+#include <string.h>
 #include "mainheader.h"
-#include "syscommon.h"
+#include "scommon.h"
 
 #define MY_PORT		60002
-#define MAXBUF		1024
+#define MAXBUF		4096
+
+static int printBuffer(char *buffer, size_t sz){
+    printf("**********************BUFFER**********************\n");
+    for(int i = 0; i < sz; i++){
+        printf(":%02X", buffer[i]);
+    }
+    printf("\n**********************END BUFFER******************\n");
+    return 1;
+}
+
+/*
+    Process the buffer. In this case, convert to an MH_ITEM_LIST_t and print.
+*/
+static int processBuffer(char *buffer, size_t sz) {
+    // TODO: if buffer size is 0, return...
+    MH_ITEM_LIST_t itemlist;
+    memset(&itemlist, 0, sizeof(MH_ITEM_LIST_t));
+    // without checking (bad), copy the buffer contents into the itemlist.
+    // NOTE: if expecting more than the one type (MH_ITEM_LIST_t), some
+    //       other functionality is needed.
+    memcpy(&itemlist, buffer, sz);
+
+    printf("**************************************************\n");
+    printf("Received buffer in driver. As an MH_ITEM_LIST_t:\n");
+    printf("\tSC_HEADER_t:\n");
+    printf("\t\tType  : %i\n", itemlist.header.type);
+    printf("\t\tStatus: %i\n", itemlist.header.status);
+    printf("\t\tCode  : %u\n", itemlist.header.code);
+    printf("\t\tLength: %u (num items in list)\n", itemlist.header.length);
+    for (int i = 0; i < itemlist.header.length; i++){
+        printf("\tMH_LIST_ITEM_t[%i]:\n", i);
+        printf("\t\tItem Type    : %i\n", itemlist.itemList[i].itemType);
+        printf("\t\tSC Msg Type  : %i\n", itemlist.itemList[i].scMsgType);
+        printf("\t\tName         : %s\n", itemlist.itemList[i].nameStr);
+    }
+    printf("**************************************************\n");
+    return 1;
+}
 
 int main(int Count, char *Strings[]) {
   // server vars
@@ -65,10 +103,24 @@ int main(int Count, char *Strings[]) {
 
   while (1) {
     // TODO: need to send a signal to break this loop from a client
+    //MH_ITEM_LIST_t itemlist;
+    size_t recsz = 0;
 
-    // Echo back anything sent for now.
-    send(clientfd, buffer, recv(clientfd, buffer, MAXBUF, 0), 0);
+    // zero the buffer
+    memset(buffer, 0, MAXBUF);
 
+    // TODO: Add some error checking...
+    recsz = recv(clientfd, buffer, MAXBUF, 0);
+
+
+    // now process the received stuff
+    if(recsz > 0){
+        printBuffer(buffer, recsz);
+        processBuffer(buffer, recsz);
+    }
+
+    // send back what we got
+    send(clientfd, buffer, recsz, 0);
   }
   // Close client connection
   close(clientfd);
